d4bcdd502fbbacae4f9bc1f1bff42c15ec4af7a5
Santiago Hadad <santihadad.sh@hotmail.com>
2020-08-13 19:34:57 -0300

    Palos y valores de cartas
    Funcion para generar valores de cartas aleatorios y mejorar las posibilidades del programa

--- laboratioro-03/practica03JuegoCartas.java
+++ laboratioro-03/practica03JuegoCartas.java
@@ -1,52 +1,78 @@
 public class practica03JuegoCartas {
     public static void main(String[] args) {
-        int contadorRondas, puntajeJugador1, puntajeJugador2, cartaJugador1, cartaJugador2;
+        int contadorRondas, puntajeJugador1, puntajeJugador2, cartaJugador1, cartaJugador2, paloJugador1, paloJugador2;
+        String paloJugador1Texto, paloJugador2Texto;
         contadorRondas = 1;
         puntajeJugador1 = puntajeJugador2 = 0;
+        paloJugador1Texto = paloJugador2Texto = "";
         while (contadorRondas <= 2) {
 
             System.out.println("Ronda N: " + contadorRondas);
 
-            cartaJugador1 = (int) (Math.random() * 4 + 1);
+            paloJugador1 = (int) (Math.random() * 4 + 1);
+            cartaJugador1 = (int) (Math.random() * 12 + 1);
+            paloJugador2 = (int) (Math.random() * 4 + 1);
+            cartaJugador2 = (int) (Math.random() * 12 + 1);
 
-            cartaJugador2 = (int) (Math.random() * 4 + 1);
-
-            switch (cartaJugador1) {
+            switch (paloJugador1) {
                 case 1:
-                    System.out.println("El jugador 1 tiene Copa");
+                    paloJugador1Texto = "Copa";
                     break;
                 case 2:
-                    System.out.println("El jugador 1 tiene Basto");
+                    paloJugador1Texto = "Basto";
                     break;
                 case 3:
-                    System.out.println("El jugador 1 tiene Espada");
+                    paloJugador1Texto = "Espada";
                     break;
                 case 4:
-                    System.out.println("El jugador 1 tiene Oro");
+                    paloJugador1Texto = "Oro";
                     break;
             }
 
-            switch (cartaJugador2) {
+            switch (paloJugador2) {
                 case 1:
-                    System.out.println("El jugador 2 tiene Copa");
+                    paloJugador2Texto = "Copa";
                     break;
                 case 2:
-                    System.out.println("El jugador 2 tiene Basto");
+                    paloJugador2Texto = "Basto";
                     break;
                 case 3:
-                    System.out.println("El jugador 2 tiene Espada");
+                    paloJugador2Texto = "Espada";
                     break;
                 case 4:
-                    System.out.println("El jugador 2 tiene Oro");
+                    paloJugador2Texto = "Oro";
                     break;
             }
-            puntajeJugador1 += cartaJugador1;
-            puntajeJugador2 += cartaJugador2;
+
+            if (cartaJugador1 > cartaJugador2) {
+                puntajeJugador1 = puntajeJugador1 + cartaJugador1;
+            } else if (cartaJugador1 < cartaJugador2) {
+                puntajeJugador2 = puntajeJugador2 + cartaJugador2;
+            } else {
+                if (paloJugador1 == 4 && paloJugador2 != 4) {
+                    puntajeJugador1 = puntajeJugador1 + cartaJugador1;
+                } else if (paloJugador2 == 4 && paloJugador1 != 4) {
+                    puntajeJugador2 = puntajeJugador2 + cartaJugador2;
+                } else {
+                    puntajeJugador1 = puntajeJugador1 + cartaJugador1;
+                    puntajeJugador2 = puntajeJugador2 + cartaJugador2;
+                }
+
+            }
+
+            if (contadorRondas == 1) {
+                System.out.println("En la primer ronda las cartas fueron: " + cartaJugador1 + " de " + paloJugador1Texto + " para el Jugador 1, y " + cartaJugador2 + " de " + paloJugador2Texto + " para el Jugador 2");
+                System.out.println("Los puntos en la primera ronda son: " + puntajeJugador1 + " para el Jugador 1, y " + puntajeJugador2 + " para el Jugador 2");
+            }
+            if (contadorRondas == 2) {
+                System.out.println("En la segunda ronda las cartas fueron: " + cartaJugador1 + " de " + paloJugador1Texto + " para el Jugador 1, y " + cartaJugador2 + " de " + paloJugador2Texto + " para el Jugador 2");
+                System.out.println("Los puntos acumulados de la segunda ronda son: " + puntajeJugador1 + " para el Jugador 1, y " + puntajeJugador2 + " para el Jugador 2");
+            }
 
             contadorRondas += 1;
         }
 
-        System.out.println("FIN RONDAS");
+        System.out.println("FIN DE LAS RONDAS");
         System.out.println("Puntaje Jugador 1: " + puntajeJugador1);
         System.out.println("Puntaje Jugador 2: " + puntajeJugador2);
 
